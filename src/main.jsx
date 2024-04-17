cb58716a63530b9c11c7cf19299689d178f1a9c4
Manojkadam8 <118733228+Manojkadam8@users.noreply.github.com>
2024-04-17 16:32:08 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -15,7 +15,7 @@ const router=createBrowserRouter(createRoutesFromElements(
              <Route path='/' element={<Layout />} >
             
             
-            <Route path='/RAUTECHA_SWARAJY' element={<App />} />
+            <Route path='RAUTECHA_SWARAJY/' element={<App />} />
 
 
 
