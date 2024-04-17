8cc05034df9824e38f3103409c9a7699676c15ce
Manojkadam8 <kadammanoj7200@gmail.com>
2024-04-17 16:26:46 +0530

    first

--- src/main.jsx
+++ src/main.jsx
@@ -15,7 +15,7 @@ const router=createBrowserRouter(createRoutesFromElements(
              <Route path='/' element={<Layout />} >
             
             
-            <Route path='' element={<App />} />
+            <Route path='/' element={<App />} />
 
 
 
