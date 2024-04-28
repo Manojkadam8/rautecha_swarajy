5cdc28000f36e522083c7dc75013b88514eb610b
Manojkadam8 <kadammanoj7200@gmail.com>
2024-04-28 12:58:28 +0530

    hamberger

--- src/Header/Navbar.jsx
+++ src/Header/Navbar.jsx
@@ -1,39 +1,67 @@
 import React, { useState } from 'react'
 import './Navbar.css'
-
-import { useMediaQuery } from "react-responsive";
-import BigScreen from '../Components/headerRsive/BigScreen/BigScreen';
+import { Link, NavLink } from 'react-router-dom'
+import { IoClose, IoMenu } from "react-icons/io5";
 
 const Navbar = () => {
-  const isMobileDevice = useMediaQuery({
-    query: "(min-device-width: 480px)",
-  });
+  
+const [openclass,setOpenclass]=useState("");
 
-  const isTabletDevice = useMediaQuery({
-    query: "(min-device-width: 768px)",
-  });
+ const OpenMenu=()=>{
+   setOpenclass("right0");
+ }
 
-  const isLaptop = useMediaQuery({
-    query: "(min-device-width: 1024px)",
-  });
-
-  const isDesktop = useMediaQuery({
-    query: "(min-device-width: 1200px)",
-  });
-
-  const isBigScreen = useMediaQuery({
-    query: "(min-device-width: 1201px )",
-  });
-
+ const CloseMenu=()=>{
+ 
+setOpenclass("right-100")
+}
   return (
     <>
-   {/* {isMobileDevice && <Mobile />}
-  {isTabletDevice && <>
-  <TabletMobile />
-  {isDesktop && <Desktop />}
-  {isLaptop && <Laptop />} */}
-  {isBigScreen && <BigScreen />}
+ <header className='header'>
+        <nav className='nav'>
+            
+            <div className='logo '>
+      <Link to="/" className="">
+                       <img src="./Images/RAUTECHA_SWARAJY_LOGO-removebg.png" alt=""  className=' imglogo' />
+         </Link>
+ 
 
+      </div>
+      <div className={`containe ${openclass}`} >
+      <div className="nav__close" id="nav-close" onClick={CloseMenu}>
+           <IoClose />
+         </div>
+         <hr />
+      <div className='header-menu '>
+      <NavLink to="/" className="">
+                       Home
+         </NavLink>
+      <NavLink to="/" className="">
+                       About
+         </NavLink>
+      <NavLink to="/" className="">
+                       Contact Us
+         </NavLink>
+ 
+
+      </div>
+      <div className='header-buttons'>
+      <NavLink to="/" className=" mbutton-1 ">
+                       Log In
+         </NavLink>
+ 
+         <NavLink to="/" className=" mbutton-1 ">
+                       Sign Up
+         </NavLink>
+
+      </div>
+     
+      </div>
+      <div className="nav__toggle" id="nav-toggle" onClick={OpenMenu} >
+         <IoMenu />
+       </div>
+        </nav>
+      </header>
 
 </>
 
